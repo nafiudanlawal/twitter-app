4e6b35aa0335890baa098fe71f9d2a3177b1768d
Nafiu Lawal <nafiudanlawal@gmail.com>
2020-11-29 23:30:07 +0000

    [update] completed site navigation links

--- src/right-column/Footer.js
+++ src/right-column/Footer.js
@@ -1,11 +1,18 @@
+const { default: Link } = require("./Link")
+
 const Footer = () => {
+    
     return (
-        <div>
+        <div className="Footer">
             <div className="Top">
-                <span>Privacy Policy</span>
+                <span><Link Href="#" Text="Terms of Service"/></span>
+                <span><Link Href="#" Text="Privacy Policy"/></span>
+                <span><Link Href="#" Text="Cookie Policy"/></span>
             </div>
             <div className="Bottom">
-                <span>All Rights Reserved</span>
+                <span><Link Href="#" Text="Ads info"/></span>
+                <span><Link Href="#" Text="More"/></span>
+                <span>© 2020 Twitter, Inc.</span>
             </div>
         </div>
     )
---
+++ src/right-column/Link.js
@@ -0,0 +1,7 @@
+const Link = ({Href, Text}) =>{
+    return (
+        <a className="Link" href={Href}>{Text}</a>
+    );
+}
+
+export default Link;
